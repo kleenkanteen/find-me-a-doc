82c0646105e9eb218898443ea2e8352a32e71527
yohanc3 <116668883+yohanc3@users.noreply.github.com>
2024-05-14 20:50:57 -0500

    fixed merging issues (#12)

--- src/services/db/database_manager.py
+++ src/services/db/database_manager.py
@@ -20,28 +20,17 @@ def get_clinics_info():
         .select("id, phone_number, last_call_date, last_call_success")
         .execute()
     )
-<<<<<<< HEAD
     return data
-=======
-    return data[1][0]
->>>>>>> master
+
 
 
 # If call fails, update last_call_date to (current date), last_call_success to (false), and corresponding docs values to NULL
 def update_db_on_failed_call(
-<<<<<<< HEAD
     clinic_id: int, available_male_docs=None, available_female_docs=None
 ):
 
     logger.info(f"clinicID on failed call: {clinic_id}")
 
-=======
-    clinic_id: int, 
-    available_male_docs=None, 
-    available_female_docs=None
-):
-
->>>>>>> master
     update_data = {
         "last_call_date": current_time(),
         "last_call_success": False,
@@ -49,16 +38,12 @@ def update_db_on_failed_call(
         "available_male_docs": available_male_docs,
     }
 
-<<<<<<< HEAD
     logger.debug(f"data to update: {update_data}")
 
-=======
->>>>>>> master
     data, count = (
         supabase.table("clinics").update(update_data).eq("id", clinic_id).execute()
     )
 
-<<<<<<< HEAD
     logger.info(f"\nupdated data: {data}\n")
 
     # check if there are any data values returned
@@ -70,13 +55,6 @@ def update_db_on_failed_call(
 
 def update_db_on_successful_call(
     clinic_id: int, available_male_docs: int, available_female_docs: int
-=======
-
-def update_db_on_successful_call(
-    clinic_id: int, 
-    available_male_docs: int, 
-    available_female_docs: int
->>>>>>> master
 ):
 
     data, count = (
@@ -92,13 +70,10 @@ def update_db_on_successful_call(
         .eq("id", clinic_id)
         .execute()
     )
-<<<<<<< HEAD
 
     logger.info(f"\nupdated data: {data}\n")
 
     return data[1][0]
-=======
->>>>>>> master
 
 
 def update_call_final_status(clinic_id: int, call_status: str):
@@ -110,8 +85,4 @@ def update_call_final_status(clinic_id: int, call_status: str):
         .update({"last_call_success": is_call_status_true})
         .eq("id", clinic_id)
         .execute()
-<<<<<<< HEAD
     )
-=======
-    )
->>>>>>> master
